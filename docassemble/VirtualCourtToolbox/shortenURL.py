76ea2b726f5c69cffa03693b91f0faacb63c9e82
plocket <52798256+plocket@users.noreply.github.com>
2020-07-10 15:09:40 -0400

    Finish moving url functionality to .misc

--- docassemble/VirtualCourtToolbox/shortenURL.py
+++
@@ -1,5 +0,0 @@
-#This is Jonathan's code, we just added a wrapper.
-import docassemble.base.functions
-class shortenMe:
-  def __init__(self, originalURL):
-    self.shortenedURL = docassemble.base.functions.temp_redirect(originalURL, 60*60*24*7, False, False)
