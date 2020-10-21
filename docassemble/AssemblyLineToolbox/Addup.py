84782f5805f7a014958c7677014ec348721a18bd
Lily  Yang <20231649+purplesky2016@users.noreply.github.com>
2020-10-21 10:32:06 -0400

    Addup; Bells and whistles; TextCounter.

---
+++ docassemble/AssemblyLineToolbox/Addup.py
@@ -0,0 +1,20 @@
+#Add up numeric field values from a DAList object.
+from docassemble.base.util import DAValidationError, word
+class Addup:
+  def __init__(self, listName, varName):
+    self.g(listName, varName)
+    
+  def g(self, listName, varName):
+    self.sum = 0
+    for w in listName:
+      my_dict = w.as_serializable()
+      for key, val in my_dict.items():
+        if key == varName:
+          self.sum += val
+    if self.sum == 0:
+      msg = "Make sure your field '" + varName + "' is spelled correctly and is numeric."
+      raise DAValidationError(msg)
+    else:
+      return self.sum
+          
+      
