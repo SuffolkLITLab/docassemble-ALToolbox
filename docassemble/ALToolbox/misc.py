f1cf2e74c8c18b3554df3c6828e35038f79b5ebd
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-04-26 16:56:40 -0400

    Missing include

--- docassemble/ALToolbox/misc.py
+++ docassemble/ALToolbox/misc.py
@@ -1,5 +1,6 @@
-#This is Jonathan's code, we just added a wrapper.
 import docassemble.base.functions
+import re
+
 class shortenMe:
   def __init__(self, originalURL):
     self.shortenedURL = docassemble.base.functions.temp_redirect(originalURL, 60*60*24*7, False, False)
