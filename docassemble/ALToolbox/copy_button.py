b91a812e8a862f03331e2c69f9e27bab56eb8916
Anonymous User <52798256+plocket@users.noreply.github.com>
2021-05-17 08:49:52 -0400

    Fix misuse of translation

--- docassemble/ALToolbox/copy_button.py
+++ docassemble/ALToolbox/copy_button.py
@@ -12,16 +12,16 @@ def copy_button_html (
   button_str = '<div class="al_copy">\n'
   if ( text_before != '' ):
     button_str += f'<span class="al_copy_description">{word( text_before )}</span>\n'
-  button_str += f'<input readonly class="al_copy_value" type="text" value="{word( text_to_copy )}">\n'
+  button_str += f'<input readonly class="al_copy_value" type="text" value="{ text_to_copy }">\n'
   button_str += f'<button class="btn btn-primary al_copy_button" type="button">\n'
   button_str += f'<span class="al_tooltip al_tooltip_inert">{word( tooltip_inert_text )}</span>\n'
   
   if ( tooltip_copied_text == 'Copied: ' ):
     # Our default text will show 'Copied: text that will be copied'
-    button_str += f'<span class="al_tooltip al_tooltip_active">Copied: {word( tooltip_copied_text ) + text_to_copy }</span>\n'
+    button_str += f'<span class="al_tooltip al_tooltip_active">{word( tooltip_copied_text ) + text_to_copy }</span>\n'
   else:
     # Otherwise, use the custom text handed in
-    button_str += f'<span class="al_tooltip al_tooltip_active">Copied: {word( tooltip_copied_text )}</span>\n'
+    button_str += f'<span class="al_tooltip al_tooltip_active">{word( tooltip_copied_text )}</span>\n'
   
   button_str += f'<i class="far fa-copy"></i>\n'
   button_str += f'<span>{ label }</span>\n'
