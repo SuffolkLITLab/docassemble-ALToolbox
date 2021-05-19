6d974bb6bc2ddb5580416a3e4018af3fc024049d
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-05-18 22:19:24 -0400

    Just bundled up the progressive disclosure CSS that we're using in a few interviews already

--- docassemble/ALToolbox/misc.py
+++ docassemble/ALToolbox/misc.py
@@ -65,3 +65,18 @@ def number_to_letter(n):
     n, remainder = divmod(n - 1, 26)
     string = chr(65 + remainder) + string
   return string
+
+def collapse_template(template, classname=None):
+  """
+  Insert HTML for a Bootstrap "collapse" div.
+  """
+  if classname is None:
+      classname = ' bg-light'
+  else:
+      classname = ' ' + classname.strip()
+  the_id = re.sub(r'[^A-Za-z0-9]', '', template.instanceName)
+  return """\
+<a class="collapsed" data-toggle="collapse" href="#{}" role="button" aria-expanded="false" aria-controls="collapseExample"><span class="pdcaretopen"><i class="fas fa-caret-down"></i></span><span class="pdcaretclosed"><i class="fas fa-caret-right"></i></span> {}</a>
+<div class="collapse" id="{}"><div class="card card-body{} pb-1">{}</div></div>\
+""".format(the_id, template.subject_as_html(trim=True), the_id, classname, template.content_as_html())
+
