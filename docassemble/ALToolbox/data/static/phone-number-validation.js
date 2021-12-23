aa6599c079d2556908e4e9c1a432a46a3c37fb99
Anonymous User <52798256+plocket@users.noreply.github.com>
2021-12-22 21:36:13 -0500

    Address the comments of the review of #39. Also add more commentary/explanation. It gave me a good opportunity to go into those details a little more and think about how to explain them.

--- docassemble/ALToolbox/data/static/phone-number-validation.js
+++ docassemble/ALToolbox/data/static/phone-number-validation.js
@@ -1,26 +1,50 @@
 $(document).on('daPageLoad', function(){
-  // Retrieve country code from a hidden field 
-  var country = $(".question-invisible");   
-  var country_code = country.val();  
+  /** Takes any inputs on the page with the class `al_intl_phone` and changes
+  *    them, giving the user a dropdown from which to choose a country. It
+  *    can also automatically identify the country based on the phone number.
+  *    When you use it with the docassemble custom datatype for phone numbers
+  *    that we've included in the ALToolbox, it will also validate the phone
+  *    number when the user taps to continue. The default country is 'us' for
+  *    now.
+  *
+  * This feature uses the library at https://www.npmjs.com/package/intl-tel-input.
+  *
+  * See phone_number_validation_demo.yml in this library for how to use this
+  *    feature of ALToolbox.
+  *
+  * We have discussed AsYouType input formatter, but haven't approached it yet.
+  *    Before discussing it, first see https://github.com/jackocnr/intl-tel-input/issues/346
+  *    where int-tel-input decided to get rid of their own version of auto
+  *    formatting user input and explained why. It's unclear if it's even possible.
+  *    One library to look into is https://nosir.github.io/cleave.js/. It's old,
+  *    but we might be able to use it as a model. We would want to test it
+  *    extensively with all the pitfalls that int-tel-input brings up and the
+  *    issues in the cleave repository itself.
+  *
+  * More Resources
+  *    1. What we're using: https://github.com/jackocnr/intl-tel-input
+  *    1. It uses https://www.npmjs.com/package/google-libphonenumber
+  *    1. https://github.com/google/libphonenumber/blob/master/FALSEHOODS.md
+  *    1. https://github.com/google/libphonenumber/blob/master/FAQ.md
+  */
   
-  // Set 'us' as the default if country_code has no value.
-  if (!country_code){
-     country_code = 'us'; 
-  }
-  // Loop thru all the phone input fields on the current screen
-  let phoneNodes = document.querySelectorAll( '.al-intl-phone' );  
-  for ( let node of phoneNodes ) {
-    let telObj = window.intlTelInput( node, {
-      initialCountry: country_code,        
-      utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.3/js/utils.min.js"
-    });        
+  // Loop through all the .al_intl_phone input fields on the current screen
+  let phoneNodes = document.querySelectorAll( '.al_intl_phone' );  // Class given by the CustomDataType
+  for ( var node of phoneNodes ) {
+    var telObj = window.intlTelInput( node, {
+      // The default country without any input into the plugin is 'us'
+      // Once the user puts in the phone number of another country, though,
+      // it will remember that country
+      initialCountry: 'us',
+      utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.15/js/utils.min.js"
+    });
     
-    // Save input as full number (eg +17024181234) to the backend        
-    $(node).val(telObj.getNumber());	
-
-    // Save country input text form (as opposed to digit) separately to the hidden field
-    // This value is "undefined" for US numbers, but our default value takes care of that.
-    $(country).val(telObj.getSelectedCountryData().iso2);      
+    // If the user already entered a number and comes back to the page,
+    // docassemble puts the previously saved value into the input field.
+    // Unfortunately, docassemble reformats the number and intl-tel-input
+    // thinks the number is invalid. It's complicated to describe, but you
+    // can see it if you go forward and back three times on our demo screens.
+    $(node).val(telObj.getNumber());
   };
   
 }); 
