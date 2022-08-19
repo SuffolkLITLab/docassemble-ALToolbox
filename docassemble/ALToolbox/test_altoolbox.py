d10aaf8dec2c1cbd78517d1727f702f37b3ba28e
nonprofittechy <nonprofittechy@users.noreply.github.com>
2022-08-19 18:32:08 +0000

    :art: Format Python code with psf/black

--- docassemble/ALToolbox/test_altoolbox.py
+++ docassemble/ALToolbox/test_altoolbox.py
@@ -2,6 +2,7 @@ import unittest
 from .business_days import is_business_day, get_next_business_day
 from docassemble.base.util import today
 
+
 class TestBusinessDays(unittest.TestCase):
     def test_is_business_day(self):
         self.assertFalse(is_business_day("2022-0-05"))
