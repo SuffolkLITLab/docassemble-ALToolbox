7e5b1396a9f43d8d1d98d08745eb90a86fec1fe3
nonprofittechy <nonprofittechy@users.noreply.github.com>
2022-11-03 13:08:45 +0000

    :art: Format Python code with psf/black

--- docassemble/ALToolbox/test_altoolbox.py
+++ docassemble/ALToolbox/test_altoolbox.py
@@ -1,5 +1,9 @@
 import unittest
-from .business_days import is_business_day, get_next_business_day, get_date_after_n_business_days
+from .business_days import (
+    is_business_day,
+    get_next_business_day,
+    get_date_after_n_business_days,
+)
 from docassemble.base.util import today, as_datetime
 
 
@@ -10,9 +14,10 @@ class TestBusinessDays(unittest.TestCase):
         this_years_christmas = today().replace(month=12, day=25)
         self.assertFalse(is_business_day(this_years_christmas))
 
-
     def test_get_date_after_n_business_days(self):
-        self.assertEquals(get_date_after_n_business_days("2022-12-24",5), as_datetime("2023-01-03"))
+        self.assertEquals(
+            get_date_after_n_business_days("2022-12-24", 5), as_datetime("2023-01-03")
+        )
 
 
 if __name__ == "__main__":
