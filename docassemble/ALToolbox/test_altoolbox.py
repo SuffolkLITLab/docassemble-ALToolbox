e919dc0bceea1b09e1e2974bab3c270ba3cd0a86
BryceStevenWilley <BryceStevenWilley@users.noreply.github.com>
2022-11-03 14:56:16 +0000

    :art: Format Python code with psf/black

--- docassemble/ALToolbox/test_altoolbox.py
+++ docassemble/ALToolbox/test_altoolbox.py
@@ -22,14 +22,18 @@ class TestBusinessDays(unittest.TestCase):
             get_date_after_n_business_days("2022-12-12", 3), as_datetime("2022-12-15")
         )
         self.assertEqual(
-            get_date_after_n_business_days("2022-12-12", 3,
-                add_holidays={"12-13": "fake day 1", "12-14": "fake day2"}
+            get_date_after_n_business_days(
+                "2022-12-12",
+                3,
+                add_holidays={"12-13": "fake day 1", "12-14": "fake day2"},
             ),
-            as_datetime("2022-12-19")
+            as_datetime("2022-12-19"),
         )
         self.assertEqual(
-            get_date_after_n_business_days("2022-12-24", 5, remove_holidays=["Christmas Day"]),
-            as_datetime("2022-12-30")
+            get_date_after_n_business_days(
+                "2022-12-24", 5, remove_holidays=["Christmas Day"]
+            ),
+            as_datetime("2022-12-30"),
         )
 
 
