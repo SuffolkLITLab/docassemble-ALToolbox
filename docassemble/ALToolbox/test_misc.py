96895bd72b1ca7005d7887f6bc4a5f674f9bc93e
Quinten Steenhuis <qsteenhuis@gmail.com>
2023-12-08 09:51:39 -0500

    Improve tests (don't rely on specific HTML structure)

--- docassemble/ALToolbox/test_misc.py
+++ docassemble/ALToolbox/test_misc.py
@@ -2,43 +2,39 @@ import re
 import unittest
 from unittest.mock import patch
 from .misc import button_array, ButtonDict
+import xml.etree.ElementTree as ET
 
 class TestButtonArray(unittest.TestCase):
 
     def normalize_whitespace(self, s):
         return re.sub(r'\s+', ' ', s)
-
-    def test_button_array_generates_correct_html(self):
+    
+    @patch('docassemble.ALToolbox.misc.user_has_privilege', return_value=False)
+    def test_button_array_generates_correct_html(self, mock_privilege):
         buttons = [
             ButtonDict(name="Button 1", image="image1", url="url1"),
             ButtonDict(name="Button 2", image="image2", url="url2"),
         ]
-        expected_html = self.normalize_whitespace(
-            '<div class="da-button-set da-field-buttons ">'
-            '<a class="btn btn-da btn-light btn-da btn-da-custom " href="url1">'
-            '<i class="fa fa-image1"></i> Button 1'
-            '</a>'
-            '<a class="btn btn-da btn-light btn-da btn-da-custom " href="url2">'
-            '<i class="fa fa-image2"></i> Button 2'
-            '</a>'
-            '</div>'
-        )
-        self.assertEqual(self.normalize_whitespace(button_array(buttons)), expected_html)
 
-    @patch('.misc.user_has_privilege', return_value=False)
+        # Just check it is valid HTML
+        button_array_html = button_array(buttons)
+        try:
+            ET.fromstring(button_array_html)
+            # If the parsing succeeds, the HTML is well-formed
+        except ET.ParseError:
+            # If the parsing fails, the HTML is not well-formed
+            self.fail("button_array generated malformed HTML")
+        self.assertIn("Button 1", button_array_html)
+        self.assertIn("Button 2", button_array_html)
+
+
+    @patch('docassemble.ALToolbox.misc.user_has_privilege', return_value=False)
     def test_button_array_filters_by_privilege(self, mock_privilege):
         buttons = [
             ButtonDict(name="Button 1", image="image1", url="url1", privilege="admin"),
             ButtonDict(name="Button 2", image="image2", url="url2"),
         ]
-        expected_html = self.normalize_whitespace(
-            '<div class="da-button-set da-field-buttons ">'
-            '<a class="btn btn-da btn-light btn-da btn-da-custom " href="url2">'
-            '<i class="fa fa-image2"></i> Button 2'
-            '</a>'
-            '</div>'
-        )
-        self.assertEqual(self.normalize_whitespace(button_array(buttons)), expected_html)
+        self.assertNotIn("Button 1", button_array(buttons))
 
 if __name__ == '__main__':
     unittest.main()
